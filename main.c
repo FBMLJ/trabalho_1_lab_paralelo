5218ed17363a534e025f3c96b7a8a1ef761a177f
João Pedro Loyola <joaoloyola@id.uff.br>
2022-10-08 00:14:43 -0300

    comentarios

--- main.c
+++ main.c
@@ -2,12 +2,10 @@
 #include <mpi.h>
 #include <stdlib.h>
 #include <math.h>
-#define TAM 40000
+#define TAM 40000 // tamanho do vetor principal
 #include <time.h>
 #define TAG_SEND_DIR 11
 #define TAG_SEND_ESQ 12
-
-// int vetor[TAM] = {9,8,7,6,5,4,3,2,1,0,-1,-2};
 
 /*
     ##################################################################################################
@@ -26,65 +24,37 @@
                                      
 */
 
-void comparare_exchage(int *vetor, int i, int j) {
-    int temp;
-    if (vetor[i] > vetor[j]) {
-        temp = vetor[j];
-        vetor[j] = vetor[i];
-        vetor[i] = temp;
-    }
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
 }
 
-int compare(const void * a, const void * b)
-{
-    return ( *(int*)a - *(int*)b );
-}
-  
+// quicksort
 void order_vetor(int *vetor, int tam) {
-     qsort(vetor, tam, sizeof(int), compare);
-    // for (int i = 0 ; i < tam; i++)
-    //     for (int j = 0; j < tam-1; j++){
-    //         comparare_exchage(vetor, j, j+1);}
+    qsort(vetor, tam, sizeof(int), compare);
 }
 
 /*
     ##################################################################################################
 */
 
-
 MPI_Status status;
-
 
 void split_min(int *vetor, int tam, int rank) {
     while(1) {
         int temp;
 
+        // envia maior valor para o processo seguinte e recebe o menor valor deste
         MPI_Send(&vetor[tam-1], 1, MPI_INT, rank+1, TAG_SEND_DIR, MPI_COMM_WORLD);
         MPI_Recv(&temp, 1, MPI_INT, rank+1, TAG_SEND_ESQ, MPI_COMM_WORLD, &status);
         
         if (temp < vetor[tam-1]) {
+            // ordena o novo valor, O(n)
             int i;
             for (i = tam-2; vetor[i] > temp && i>=0; i--) vetor[i+1] = vetor[i];
             vetor[i+1] = temp;
         }
         else break;
     }
-
-
-    // int tam_min = tam_padrao;
-    // int tam_max = (rank == size-2)? tam_resto : tam_padrao;
-    // int tam_aux = tam_min + tam_max;
-    // int *vetor_aux = (int*) malloc((tam_aux)*sizeof(int));
-
-    // for (int i = 0; i < tam_min; i++) vetor_aux[i] = vetor[i];
-
-    // MPI_Send(vetor, tam_min, MPI_INT, rank+1, TAG_SEND_DIR, MPI_COMM_WORLD);
-    // MPI_Recv(&vetor_aux[tam_min], tam_max, MPI_INT, rank+1, TAG_SEND_ESQ, MPI_COMM_WORLD, &status);
-
-    // order_vetor(vetor_aux, tam_aux);
-
-    // for (int i = 0; i < tam_min; i++) vetor[i] = vetor_aux[i];
-    // free(vetor_aux);
 }
 
 
@@ -92,41 +62,23 @@ void split_max(int *vetor, int tam, int rank) {
     while(1) {
         int temp;
 
+        // envia menor valor para o processo anterior e recebe o maior valor deste
         MPI_Send(vetor, 1, MPI_INT, rank-1, TAG_SEND_ESQ, MPI_COMM_WORLD);
         MPI_Recv(&temp, 1, MPI_INT, rank-1, TAG_SEND_DIR, MPI_COMM_WORLD, &status);
         
         if (temp > vetor[0]) {
+            // ordena o novo valor, O(n)
             int i;
             for (i = 1; temp > vetor[i] && i <= tam-1; i++) vetor[i-1] = vetor[i];
             vetor[i-1] = temp;
         }
         else break;
     }
-
-
-    // int tam_min = tam_padrao;
-    // int tam_max = (rank == size-1)? tam_resto : tam_padrao;
-    // int tam_aux = tam_min + tam_max;
-    // int *vetor_aux = (int*) malloc((tam_aux)*sizeof(int));
-
-    // for (int i = 0; i < tam_max; i++) vetor_aux[i] = vetor[i];
-
-    // MPI_Send(vetor, tam_max, MPI_INT, rank-1, TAG_SEND_ESQ, MPI_COMM_WORLD);
-    // MPI_Recv(&vetor_aux[tam_max], tam_min, MPI_INT, rank-1, TAG_SEND_DIR, MPI_COMM_WORLD, &status);
-
-    // order_vetor(vetor_aux, tam_aux);
-
-    // for (int i = 0; i < tam_max; i++) vetor[i] = vetor_aux[tam_min + i];
-    // free(vetor_aux);
 }
 
 
 
 int main(int argc, char** argv) {
-    // int TAM;
-    // fscanf(f,  "%d\n", )
-
-    // return 0;
     /*
     ##################################################################################################
                    ___ _   _ ___ ____ ___    _    _     ___ _____   _    ____    _    ___                                 
@@ -144,26 +96,27 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Status status;
 
-    int tam_padrao = TAM/size;
+    int tam_padrao = TAM/size; // tamanho do vetor local (sem resto)
     int inicio = rank * tam_padrao;
     int fim = inicio + tam_padrao;
     int resto = TAM % size;
-    if (rank == size-1) fim += resto;
-    int tam = fim - inicio;
-    // printf("%d  %d\n", inicio, fim);
+    if (rank == size-1) fim += resto; // resto fica no último processo
+    int tam = fim - inicio; // tamanho do vetor local para cada processo
 
     int *vetor_local = (int*) malloc(sizeof(int)*tam);
     if (rank == 0) {
+        // leitura dos TAM valores
         FILE *f = fopen("input.txt", "r");
         int *vetor = (int*) malloc(sizeof(int)*TAM);
         for(int i = 0 ; i < TAM;i++){
             int temp;
-            fscanf(f,"%d", &temp);
-            // printf("%d\n", temp);
+            fscanf(f, "%d", &temp);
             vetor[i] = temp;
         }
 
+        // distribuição do vetor principal pelos vetores locais dos processos
         for (int i = 1; i < size; i++) {
+            // define o tamanho da mensagem de acordo com o seu destino
             int tamanho = (i == size-1)? tam_padrao+resto : tam_padrao;
             MPI_Send(&vetor[i*tam_padrao], tamanho, MPI_INT, i, 0, MPI_COMM_WORLD);
         }
@@ -172,6 +125,7 @@ int main(int argc, char** argv) {
         free(vetor);
     } 
     else {
+        // distribuição do vetor principal pelos vetores locais dos processos
         MPI_Recv(vetor_local, tam, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
     }
 
@@ -179,28 +133,29 @@ int main(int argc, char** argv) {
     ######################################################################################################
     */
     
+    // número de iterações extras (necessárias quando tem resto)
     int extra = ceil((float)(resto) / tam_padrao);
 
+    // ordenação sequencial dos vetores locais
     order_vetor(vetor_local, tam);
     
-    for (int interator_process = 0 ; interator_process < size + extra; interator_process++) {
-        // printf("TESTE\n");
-        if (interator_process % 2 == 1) {
+    for (int interator_process = 0; interator_process < size+extra; interator_process++) {
+        if (interator_process % 2 == 1) { // iteração impar
             if (rank % 2 == 1) { //rank impar
                 if (rank < size-1) 
                     split_min(vetor_local, tam, rank);
             }
-            else {
+            else { //rank par
                 if (rank > 0)
                     split_max(vetor_local, tam, rank);
             }
         }
-        else { // interador par
-            if (rank % 2 == 0 ) {
+        else { // iteração par
+            if (rank % 2 == 0 ) { //rank par
                 if (rank < size-1) 
                     split_min(vetor_local, tam, rank);
             }
-            else {//rank impar
+            else { //rank impar
                 split_max(vetor_local, tam, rank);
             }
         }
@@ -211,7 +166,9 @@ int main(int argc, char** argv) {
         for (int i = 0; i < tam; i++) vetor[i] = vetor_local[i];
         free(vetor_local);
 
+        // junção dos vetores locais no processo 0
         for (int i = 1; i < size; i++) {
+            // define o tamanho da mensagem de acordo com a sua origem
             int tam_rank = (i == size-1)? tam_padrao+resto : tam_padrao;
             MPI_Recv(&vetor[i * tam_padrao], tam_rank, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
         }
@@ -219,17 +176,19 @@ int main(int argc, char** argv) {
         // tempo de execução
         clock_t end = clock();
         time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-        // printf("The elapsed time is %f seconds\n", time_spent);
-        // escrevendo resultado no arquivo
+        
+        // escreve vetor ordenado em um arquivo
         FILE * f = fopen("output.txt", "w");
         for (int i = 0; i < TAM; i++) fprintf(f, "%d  ", vetor[i]);
         free(vetor); fclose(f);
 
+        // escreve tempo de execução em um arquivo
         FILE *f1 = fopen("time_result.csv", "a");
-        fprintf(f1,"%f\n", time_spent);
+        fprintf(f1, "%f\n", time_spent);
         fclose(f1);
     } 
     else {
+        // junção dos vetores locais no processo 0
         MPI_Send(vetor_local, tam, MPI_INT, 0, 0, MPI_COMM_WORLD);
         free(vetor_local);
     }
@@ -237,6 +196,4 @@ int main(int argc, char** argv) {
     MPI_Finalize();
     
     return 0;
-
-    // printf()
 }
